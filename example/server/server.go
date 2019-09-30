f19c50a7605af80b937c41f315b10ae75f1fcadc
joyoes <458237671@qq.com>
2019-09-30 22:47:20 +0800

    joyoes first commit

---
+++ example/server/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gogf/gf-jwt/example/auth"
+	"github.com/gogf/gf/frame/g"
+	"github.com/gogf/gf/net/ghttp"
+)
+
+// hello should be authenticated to view.
+func hello(r *ghttp.Request) {
+	r.Response.Write("Hello World!")
+}
+
+// works is the default router handler for web server.
+func works(r *ghttp.Request) {
+	r.Response.Write("It works!")
+}
+
+// authHook is the HOOK function implements JWT logistics.
+func authHook(r *ghttp.Request) {
+	r.Response.CORSDefault()
+	auth.GfJWTMiddleware.MiddlewareFunc()(r)
+}
+
+func main() {
+	s := g.Server()
+	s.Group("/").Bind([]ghttp.GroupItem{
+		{"ALL",  "/",           works},
+		{"POST",  "/login",     auth.GfJWTMiddleware.LoginHandler},
+	})
+	s.Group("/user").Bind([]ghttp.GroupItem{
+		{"ALL", "*",              authHook, ghttp.HOOK_BEFORE_SERVE},
+		{"GET", "/refresh_token", auth.GfJWTMiddleware.RefreshHandler},
+		{"GET", "/hello",         hello},
+	})
+	s.SetPort(8000)
+	s.Run()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/gogf/gf-jwt
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gogf/gf v1.9.6
+)
+
+go 1.13
